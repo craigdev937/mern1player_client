53cdea56eabe9bfd9fe00f3888ea316870b8d36e
Craig <indigomx9@gmail.com>
2023-08-28 11:13:39 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RTK-Query and Cloudinary
+
+Initial setup of the project.  Created with TypeScript, React, React Router, and Redux-Toolkit featuring RTK-Query.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et itaque quos vero corrupti impedit accusamus illum officiis quia maxime totam fuga, consequatur non repellendus ex ea maiores, libero ipsum sint?</p>
+        </React.Fragment>
+    );
+};
+
+
